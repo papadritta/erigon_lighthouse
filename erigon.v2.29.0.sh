9717c2e7f53a2ab7e12e468059ac008a2136af41
Papadritta <90826754+papadritta@users.noreply.github.com>
2022-11-06 21:31:48 +0700

    create erigon.v2.29.0.sh
    new update erigon.v2.29.0

---
+++ erigon.v2.29.0.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt update -y && apt upgrade -y && apt autoremove -y
+
+sudo systemctl stop erigon
+sudo rm -rf /usr/local/bin/erigon
+
+cd $HOME
+curl -LO https://github.com/ledgerwatch/erigon/archive/refs/tags/v2.29.0.tar.gz
+tar xvf v2.29.0.tar.gz
+cd erigon-2.29.0
+make erigon
+
+cd $HOME
+sudo mv erigon-2.29.0 /usr/local/bin/erigon
+
+rm v2.29.0.tar.gz
+
+chmod +x /usr/local/bin/erigon
+sudo chown -R erigon:erigon /var/lib/erigon
+
+sudo systemctl restart erigon
+
+sudo journalctl -fu erigon
