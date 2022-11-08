87721bf0a79f1b3544a6d9936ac2745cbe0e37a5
Papadritta <90826754+papadritta@users.noreply.github.com>
2022-11-08 19:40:28 +0700

    update sh
    add pre-build binary lighthouse-v3.2.1

--- erigon.v2.29.0.sh
+++ erigon.v2.29.0.sh
@@ -2,7 +2,9 @@
 apt update -y && apt upgrade -y && apt autoremove -y
 
 sudo systemctl stop erigon
+sudo systemctl stop lighthousebeacon
 sudo rm -rf /usr/local/bin/erigon
+sudo rm -rf /usr/local/bin/lighthouse
 
 cd $HOME
 curl -LO https://github.com/ledgerwatch/erigon/archive/refs/tags/v2.29.0.tar.gz
@@ -18,6 +20,20 @@ rm v2.29.0.tar.gz
 chmod +x /usr/local/bin/erigon
 sudo chown -R erigon:erigon /var/lib/erigon
 
-sudo systemctl restart erigon
+cd $HOME
+curl -LO https://github.com/sigp/lighthouse/releases/download/v3.2.1/lighthouse-v3.2.1-x86_64-unknown-linux-gnu.tar.gz
+tar xvf lighthouse-v3.2.1-x86_64-unknown-linux-gnu.tar.gz
+sudo mv lighthouse /usr/local/bin
 
-sudo journalctl -fu erigon
+rm lighthouse-v3.2.1-x86_64-unknown-linux-gnu.tar.gz
+
+chmod +x /usr/local/bin/lighthouse
+sudo chown -R lighthousebeacon:lighthousebeacon /var/lib/lighthouse/beacon
+
+sudo systemctl restart erigon
+sudo systemctl restart lighthousebeacon
+
+sudo systemctl status erigon
+sudo systemctl status lighthousebeacon
+
+
